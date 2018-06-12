af4b88637a9014bc9d5636d0c05ecf37ea0d0381
Sky Wilson <swilson@kanaitek.com>
2018-06-12 15:10:35 -0700

    Building components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { CompanyAuthComponent } from '../components/company-auth/company-auth';
 
 @Component({
   templateUrl: 'app.html'
