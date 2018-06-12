853737b2e15729031cdd31a346b44aebc1d0635e
Sky Wilson <swilson@kanaitek.com>
2018-06-12 08:35:49 -0700

    Inspection form testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { File } from '@ionic-native/file';
+import { IonicStorageModule } from '@ionic/storage';
+import { Camera } from '@ionic-native/camera';
+import { FileTransfer } from '@ionic-native/file-transfer';
+import { NativePageTransitions } from '@ionic-native/native-page-transitions';
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
@@ -11,16 +19,21 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { InspectionFormComponent } from '../components/inspection-form/inspection-form';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    InspectionFormComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,11 +42,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    InspectionFormComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    File,
+    Camera,
+    FileTransfer,
+    NativePageTransitions,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
---
+++ src/components/inspection-form/inspection-form.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, LoadingController, ToastController, AlertController } from 'ionic-angular';
+import { NgForm } from '@angular/forms';
+
+import { Camera } from '@ionic-native/camera';
+import { File, Entry, FileError } from '@ionic-native/file';
+
+// import { AuthService } from'../../services/auth.service';
+// import { DriverService } from '../../services/driver.service';
+// import { InspectionService } from '../../services/inspection.service';
+//
+// import { TripsPage } from '../schedule/trips/trips';
+
+declare var cordova: any;
+
+@Component({
+  selector: 'inspection-form',
+  templateUrl: 'inspection-form.html',
+})
+export class InspectionFormComponent {
+  token: string;
+  driver: string;
+  company: string;
+  imageUrl = '';
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private loadingCtrl: LoadingController, private alertCtrl: AlertController,
+              private camera: Camera, private toastCtrl: ToastController, private file: File) {
+    // this.initDriver();
+    // this.onLoadToken();
+  }
+
+  onOpenCamera() {
+      this.camera.getPicture({
+        correctOrientation: true,
+      })
+        .then((imageData) => {
+          // imageData depends on save destination type
+          console.log(imageData);
+          const currentName = imageData.replace(/^.*[\\\/]/, '');
+          const path = imageData.replace(/[^\/]*$/, '');
+          const newFileName = new Date().getUTCMilliseconds() + '.jpg';
+          // cordova.file.dataDirectory == folder for this app on the specific platform/device where you can store files permanently
+          // File.moveFile(sourcePath, sourceFileName, destinationPath, destinationFileName)
+          this.file.moveFile(path, currentName, cordova.file.dataDirectory, newFileName)
+            .then((data: Entry) => {
+              this.imageUrl = data.nativeURL;
+            })
+            .catch((error: FileError) => {
+              this.imageUrl = '';
+              const toast = this.toastCtrl.create({
+                message: 'Could not save image. Please try again',
+                duration: 2500
+              });
+              toast.present();
+              // Cleanup renames each file same name - must set new file name,'newFileName'
+              this.camera.cleanup();
+              // OR
+              // File.removeFile(path, currentName);
+
+            });
+          this.imageUrl = imageData;
+        })
+        .catch((error) => {
+          const toast = this.toastCtrl.create({
+            message: 'Could not take photo. Please try again',
+            duration: 2500
+          });
+          toast.present();
+          this.camera.cleanup();
+        });
+    }
+
+    onSubmit(form: NgForm) {
+      const loading = this.loadingCtrl.create({
+        content: 'Submitting your vehicle inspection...'
+      });
+      loading.present();
+
+    }
+
+}
