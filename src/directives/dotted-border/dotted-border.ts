8c1a3c34668705992346edd58e0d64d4031eefa4
Skyler Wilson <skyler@Randys-iMac-2.local>
2018-06-19 09:50:17 -0700

    Working on svg

--- src/directives/dotted-border/dotted-border.ts
+++ src/directives/dotted-border/dotted-border.ts
@@ -7,13 +7,14 @@ import { OnInit, Directive, HostBinding } from '@angular/core';
 export class DottedBorderDirective implements OnInit{
   // Even EASIER than Renderer2 - binds a variable to any property of the DOM element the directive is sitting on
   @HostBinding('style.background') bg: string;
+  svgPath = '../../../assets/svg/dotted_border.svg';
 
   constructor() {
 
   }
 
   ngOnInit() {
-    this.bg = "url(../../../assets/svg/dotted_border.svg)";
+    this.bg = 'url(' + this.svgPath + ')';
   }
 
 }
