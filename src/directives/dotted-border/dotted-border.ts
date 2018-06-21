293777b54836001c59594eb9e64b27990894fecd
Skyler Wilson <skyler@Randys-iMac-2.local>
2018-06-21 12:22:49 -0700

    Making route card UI colors a function of route type state

--- src/directives/dotted-border/dotted-border.ts
+++ src/directives/dotted-border/dotted-border.ts
@@ -1,10 +1,10 @@
-import { OnInit, Directive, HostBinding, Input } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 
 @Directive({
   selector: '[dotted-border]' // Attribute selector
 })
-export class DottedBorderDirective implements OnInit {
+export class DottedBorderDirective {
   // Even EASIER than Renderer2 - binds a variable to any property of the DOM element the directive is sitting on
   @HostBinding('style.background') bg: string;
   @HostBinding('style.backgroundSize') bgSize: string;
@@ -15,25 +15,14 @@ export class DottedBorderDirective implements OnInit {
   @Input() set svgType(type: string) {
     if(type==='dropOff') {
       this.svgPath = 'assets/svg/dotted_border_alt.svg';
+      this.bg = 'url(' + this.svgPath + ') 75% 75% no-repeat center';
     } else {
       this.svgPath = 'assets/svg/dotted_border.svg';
+      this.bg = 'url(' + this.svgPath + ') 75% 75% no-repeat center';
     }
-    this.initStyles();
   }
 
   constructor() {
 
   }
-
-  ngOnInit() {
-    this.initStyles();
-  }
-
-  initStyles() {
-    this.bg = 'url(' + this.svgPath + ')';
-    this.bgSize = '75% 75%';
-    this.bgRepeat = 'no-repeat';
-    this.bgPos = 'center';
-  }
-
 }
