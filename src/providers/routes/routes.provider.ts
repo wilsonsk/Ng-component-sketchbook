4126a48c8ab48149c2f8aaae2f9fda1b33cc7dcb
Skyler Wilson <skyler@Randys-iMac-2.local>
2018-06-28 10:27:17 -0700

    Testing ng template that fires when routes.length <= 0

--- src/providers/routes/routes.provider.ts
+++ src/providers/routes/routes.provider.ts
@@ -9,11 +9,11 @@ import { RouteState } from './route-state.model';
 export class RoutesProvider {
   // Test Route Data
   routes: RouteModel[] = [
-    new RouteModel('05:50AM', 1, 'source address 1', '1800 NE Alberta St, Portland, OR 97211', '06:40AM', true, false, null, 'p', null, null, null, null, null),
-    new RouteModel('07:20AM', 2, 'source address 2', '8801 NE Hazel Dell Ave, Vancouver, WA 98665', '10:20AM', true, true, null, 'p', null, null, null, null, null),
-    new RouteModel('01:15PM', 3, 'source address 3', '8801 NE Hazel Dell Ave, Vancouver, WA 98665', '02:00PM', false, false, null, 'd', null, null, null, null, null),
-    new RouteModel('03:25PM', 4, 'source address 4', '2211 NE 139th St, Vancouver, WA 98686', '04:20PM', false, false, null, 'd', null, null, null, null, null),
-    new RouteModel('05:50PM', 5, 'source address 5', '700 NE 87th Ave, Vancouver, WA 98664', '07:00PM', true, true, null, 'p', null, null, null, null, null),
+    // new RouteModel('05:50AM', 1, 'source address 1', '1800 NE Alberta St, Portland, OR 97211', '06:40AM', true, false, null, 'p', null, null, null, null, null),
+    // new RouteModel('07:20AM', 2, 'source address 2', '8801 NE Hazel Dell Ave, Vancouver, WA 98665', '10:20AM', true, true, null, 'p', null, null, null, null, null),
+    // new RouteModel('01:15PM', 3, 'source address 3', '8801 NE Hazel Dell Ave, Vancouver, WA 98665', '02:00PM', false, false, null, 'd', null, null, null, null, null),
+    // new RouteModel('03:25PM', 4, 'source address 4', '2211 NE 139th St, Vancouver, WA 98686', '04:20PM', false, false, null, 'd', null, null, null, null, null),
+    // new RouteModel('05:50PM', 5, 'source address 5', '700 NE 87th Ave, Vancouver, WA 98664', '07:00PM', true, true, null, 'p', null, null, null, null, null),
     new RouteModel('07:50PM', 5, 'source address 5', '700 NE 87th Ave, Vancouver, WA 98664', '08:30PM', true, true, null, 'd', null, null, null, null, null),
   ];
 
